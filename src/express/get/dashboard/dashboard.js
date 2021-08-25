3c13e1cba746c2c04689d0deee002bae41fa2e74
404invalid-user <user@invaliduser.uk.to>
2021-08-25 19:02:33 +0100

    fixed error

--- src/express/get/dashboard/dashboard.js
+++ src/express/get/dashboard/dashboard.js
@@ -81,7 +81,7 @@ module.exports = {
                     if (userGuild.owner == true) {
                         guilds.push({ id: userGuild.id, name: userGuild.name, icon: `https://cdn.discordapp.com/icons/${userGuild.id}/${userGuild.icon}.webp`, mutual: true, userPermission: 'owner' });
                     } else {
-                        if (client.guilds.cache.get(currentServer.id).members.find(currentUser.userId).roles.cache.find(r => currentServer.staffRoles.includes(r.name))) {
+                        if (client.guilds.cache.get(currentServer.id).members.find(userInfo.userId).roles.cache.find(r => currentServer.staffRoles.includes(r.name))) {
                             guilds.push({ id: userGuild.id, name: userGuild.name, icon: `https://cdn.discordapp.com/icons/${userGuild.id}/${userGuild.icon}.webp`, mutual: true, userPermission: 'staffrole' });
                         } else {
                             currentServer.staff.forEach(userid => {
