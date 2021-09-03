40d2a4ac4fd18bf3917e59140823408ea38225bc
404invalid-user <user@invalidlag.com>
2021-09-03 04:44:33 +0100

    Update getuser.js

--- src/funcs/getuser.js
+++ src/funcs/getuser.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 module.exports = async(message) => {
-    let currentUser = await User.findOne({ id: message.author.id, guildID: message.guild.id });
+    let currentUser = await User.findOne({ userid: message.author.id, guildID: message.guild.id });
     if (currentUser == null) {
         await User.create({
             userid: message.author.id,
